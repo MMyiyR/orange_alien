410b18f435f5e15d9cb338b5c6eaee3db179ea09
MMyiyR <63156852+MMyiyR@users.noreply.github.com>
2020-05-09 02:27:45 +0900

    Add files via upload

---
+++ mikannseijinn.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  $('.letter').hover(
+    function() {
+      $('#social').css('opacity', '0.8');
+      $('#social').css('transition', 'all 150ms 0s ease');
+    },
+    function() {
+      $('#social').css('opacity', '0');
+    }
+  )
+
+});
