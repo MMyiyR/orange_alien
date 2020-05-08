057ce61eb466713c61fdd2dfaf58b8faaf218288
MMyiyR <63156852+MMyiyR@users.noreply.github.com>
2020-05-09 02:49:39 +0900

    Update mikannseijinn.js

--- mikannseijinn.js
+++ mikannseijinn.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $('.letter').hover(
+  $('.letters').hover(
     function() {
       $('#social').css('opacity', '0.8');
       $('#social').css('transition', 'all 150ms 0s ease');
