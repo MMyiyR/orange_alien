f5fa20d3dc389ede74871b6835211e0f8b43459e
MYIYM <63156852+MYIYM@users.noreply.github.com>
2020-07-05 16:06:04 +0900

    Update orange_alien_web_ver1.js

--- orange_alien_web_ver1.js
+++ orange_alien_web_ver1.js
@@ -1,24 +1,39 @@
 $(function() {
-$('body').fadeIn();
+
+
+  if ($(window).width() > 520) {
+    $('.OREN').animate({'opacity':'1'},2000,function(){
+      $('.logo-OREN').animate({'height':'0'},1000,function(){
+        $('.all').fadeIn(1000).dequeue();
+      });
+    });
+
+  } else {
+    $('.OREN').animate({'opacity':'1'},2000,function(){
+      $('.logo-OREN').animate({'height':'5%','width': '25%'},1000,function(){
+        $('.all').fadeIn(1000).dequeue();
+      });
+    });
+
+  }
+
 
 
 var opacity1 = function() {
-  $('#fadeing1').animate({'opacity':'0.8'},1000,function(){
+  $('#fadeing1').animate({'opacity':'0.6'},1000,function(){
     $('#fadeing1').animate({'opacity':'1'},1000).dequeue();
     setTimeout(opacity1, 1000);
-  })
+  });
 }
 opacity1();
 
 var opacity2 = function() {
-  $('#fadeing2').animate({'opacity':'0.8'},1000,function(){
+  $('#fadeing2').animate({'opacity':'0.6'},1000,function(){
     $('#fadeing2').animate({'opacity':'1'},1000).dequeue();
     setTimeout(opacity2, 1000);
-  })
+  });
 }
 opacity2();
-
-
 
   $('.home').click(function() {
     $('.active').removeClass('active');
@@ -56,16 +71,17 @@ opacity2();
 
   });
 
-  $('.favs').click(function() {
+  $('.M----').click(function() {
     $('.active').removeClass('active');
-    $('.favs').addClass('active');
+    $('.M----').addClass('active');
 
   	$('.mains').animate({'margin-left':'10px','opacity':'0'},150,function(){
       $('.mains').removeClass('block');
-      $('.favs-main').addClass('block');
+      $('.M-----main').addClass('block');
       $('.mains').animate({'margin-left':'0px','opacity':'1'},150).dequeue();
 	  });
 
   });
 
 });
+
