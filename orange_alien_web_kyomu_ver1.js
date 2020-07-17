4649d9e60119e9e905a06c3e4379517b9f076955
MYIYM <63156852+MYIYM@users.noreply.github.com>
2020-07-17 11:07:55 +0900

    Update orange_alien_web_kyomu_ver1.js

--- orange_alien_web_kyomu_ver1.js
+++ orange_alien_web_kyomu_ver1.js
@@ -1,31 +1,23 @@
 $(function() {
 
+
   $('.kyomu-icon1').click(function() {
     if ($('.kyomu-icon1').hasClass('mu')) {
       // 閉じる
-      $('.mikan-background-color').slideUp(50);
-      $('.mikan-ue').slideUp(70);
-      $('.kyomu-icon1').removeClass('mu');
-    } else {
-       // 開く
-      $('.mikan-background-color').slideDown(50);
-      $('.mikan-ue').slideDown(70);
-      $('.kyomu-icon1').addClass('mu');
-    }
-  });
-
-  $('.kyomu-icon2').click(function() {
-    if ($('.kyomu-icon2').hasClass('mu')) {
-      // 閉じる
       $('.mikan-background-color').hide();
       $('.mikan-yoko').hide();
-      $('.kyomu-icon2').removeClass('mu');
+      $('.kyomu-icon1').removeClass('mu');
     } else {
        // 開く
       $('.mikan-background-color').show();
       $('.mikan-yoko').show();
-      $('.kyomu-icon2').addClass('mu');
+      $('.kyomu-icon1').addClass('mu');
     }
+  });
+  $('.mikan-background-color').click(function() {
+    $('.mikan-background-color').hide();
+    $('.mikan-yoko').hide();
+    $('.kyomu-icon1').removeClass('mu');
   });
 
 });
